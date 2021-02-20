7b741476524dc56bc862fd0b5c4d290caaf98449
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2021-02-20 23:13:37 +0100

    Layout scanner page

--- src/pods/camera/camera.component.tsx
+++ src/pods/camera/camera.component.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import Webcam from "react-webcam";
+
+const image = require("../../assets/light.svg");
+
+//CSS
+import * as classes from "./camera.styles";
+
 export const CameraComponent: React.FunctionComponent = () => {
+  const webcamRef = React.useRef(null);
+
   return (
     <>
-      <h1>Camera component</h1>
+      <div className={classes.container}>
+        <h1 className={classes.h1}>Take picture</h1>
+        <p className={classes.text}>Fit your ID card inside the frame.</p>
+        <p className={classes.text}>The picture will be taken automatically.</p>
+        <div className={classes.cameraContainer}>
+          <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        </div>
+        <div className={classes.message}>
+          <img src={image}></img>
+          <p className={classes.textMessage}>Room lighting is too low</p>
+        </div>
+        <div className={classes.buttonContainer}>
+          <button className={classes.button}>CANCEL</button>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/pods/camera/camera.styles.ts
@@ -0,0 +1,77 @@
+import { css } from "emotion";
+
+//CSS
+import styles from "../../mystyles.scss";
+
+export const container = css`
+  background-color: #1d1d1d;
+  overflow: auto;
+  height: 100vh;
+  padding: 0 2em 0 2em;
+`;
+
+export const h1 = css`
+  margin-top: 3em;
+  padding: 1em;
+  font-family: ${styles.principalFont};
+  color: #ffffff;
+  font-size: 21px;
+  font-weight: bold;
+  letter-spacing: 0;
+  line-height: 24px;
+  text-align: center;
+`;
+
+export const text = css`
+  color: #ffffff;
+  font-family: ${styles.principalFont};
+  font-size: 16px;
+  letter-spacing: 0;
+  line-height: 24px;
+  text-align: center;
+`;
+
+export const cameraContainer = css`
+  height: 200px;
+  margin-top: 4em;
+  border: 2px solid red;
+  border-radius: 20px;
+  overflow: auto;
+  video {
+    width: 100%;
+  }
+`;
+
+export const buttonContainer = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5em;
+`;
+
+export const button = css`
+  background: none;
+  border: none;
+  color: #ffffff;
+  font-family: ${styles.principalFont};
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 0.35px;
+  line-height: 24px;
+  text-align: center;
+`;
+
+export const message = css`
+  margin-top: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const textMessage = css`
+  font-family: ${styles.principalFont};
+  color: #ffffff;
+  font-size: 16px;
+  letter-spacing: 0;
+  line-height: 16px;
+`;
